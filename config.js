8396f9c4cae8255f9276b99d430e8259399d3014
Mielsita <91367015+Mielsita@users.noreply.github.com>
2021-10-17 14:50:37 -0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
+  name: 'Emma',
   imageBackground: false,
   openInNewTab: true,
   twelveHourFormat: false,
@@ -25,7 +25,7 @@ const CONFIG = {
   // Weather
   weatherKey: 'InsertYourAPIKeyHere123456',
   weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
-  weatherUnit: 'C', // 'F', 'C'
+  weatherUnit: 'F', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
