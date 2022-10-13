3703aaf04643831e7bec185af01e90a7a83b2804
Mielsita <91367015+Mielsita@users.noreply.github.com>
2022-10-13 09:08:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,15 @@ const CONFIG = {
   twelveHourFormat: false,
 
   // Greetings
-  greetingMorning: 'Good morning genius!',
-  greetingAfternoon: 'Grind time foolish',
-  greetingEvening: 'Hello Emma!',
-  greetingNight: 'Go the fk to bed',
+  //greetingMorning: 'Good morning genius!',
+  //greetingAfternoon: 'Grind time foolish',
+  //greetingEvening: 'Hello Emma!',
+  //greetingNight: 'Go the fk to bed',
+  
+  greetingMorning: 'Happy birthday genius!',
+  greetingAfternoon: 'Happy birthday, I hope you have the best day',
+  greetingEvening: 'Happy birthday, You are amazing, and now 21!',
+  greetingNight: 'I hope you had a great birthday!',
 
   // Weather
   weatherKey: 'bb30b901d29ed3093dcc1381ce592da8',
